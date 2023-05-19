8cb602f58428b47214d6139e09d40ee56838c28d
ayesha8221 <ayesha.glnt012@gmail.com>
2023-05-19 15:15:00 +0200

    mycalc

---
+++ script/index.js
@@ -0,0 +1,80 @@
+
+
+// Clear on  click AC
+function clr() {
+    document.getElementById('result').value= ""
+}
+
+// delete numbers on click
+
+// input numbers on click
+
+function num7(){
+    document.getElementById('result').value+="7"
+}
+
+function num8(){
+    document.getElementById('result').value+="8"
+}
+
+function num9(){
+    document.getElementById('result').value+="9"
+}
+
+function num4(){
+    document.getElementById('result').value+="9"
+}
+
+function num5(){
+    document.getElementById('result').value+="5"
+}
+
+function num6(){
+    document.getElementById('result').value+="6"
+}
+
+function num1(){
+    document.getElementById('result').value+="1"
+}
+
+function num2(){
+    document.getElementById('result').value+="2"
+}
+
+function num3(){
+    document.getElementById('result').value+="3"
+}
+
+function num0(){
+    document.getElementById('result').value+="0"
+}
+
+function dot(){
+    document.getElementById('result').value+="."
+}
+
+function sum() {
+        document.getElementById('result').value+="+"
+}
+
+function min() {
+        document.getElementById('result').value+="-"
+}
+
+function divide() {
+        document.getElementById('result').value+="/"
+}
+
+function multi() {
+        document.getElementById('result').value+="*"
+}
+
+function percent() {
+        document.getElementById('result').value+="%"
+}
+
+
+
+
+
+
